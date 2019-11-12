6e3e746005cdc4b647b1e9134206710ab5e3272c
Tomohiro Matsuzawa <thmatuza75@hotmail.com>
2019-11-12 19:00:36 +0900

    rm ignore files

--- Pods/Target Support Files/Pods-tencent-broadcast/Pods-tencent-broadcast-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tencent_broadcast : NSObject
-@end
-@implementation PodsDummy_Pods_tencent_broadcast
-@end
--- Pods/Target Support Files/VideoCast/VideoCast-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VideoCast : NSObject
-@end
-@implementation PodsDummy_VideoCast
-@end
